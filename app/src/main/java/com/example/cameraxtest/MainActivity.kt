a238cc70a7d36c34b9bad9f98f0c83ca537474e5
leonardo1710 <leon.freudenthaler@fh-campuswien.ac.at>
2023-05-07 14:00:59 +0200

    working camerax

--- app/src/main/java/com/example/cameraxtest/MainActivity.kt
+++ app/src/main/java/com/example/cameraxtest/MainActivity.kt
@@ -1,33 +1,62 @@
 package com.example.cameraxtest
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
 import com.example.cameraxtest.ui.theme.CameraXTestTheme
+import com.example.cameraxtest.Commons.REQUIRED_PERMISSIONS
+import com.example.cameraxtest.Commons.allPermissionsGranted
 
 class MainActivity : ComponentActivity() {
+
+    private var cameraX: CameraX = CameraX(this, this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CameraXTestTheme {
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CameraCompose(this, cameraX = cameraX) {
+                        if (allPermissionsGranted(this)) {
+                            cameraX.capturePhoto()
+                        }
+                    }
                 }
             }
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String) {
@@ -39,5 +68,53 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     CameraXTestTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun CameraCompose(
+    context: Context,
+    cameraX: CameraX,
+    onCaptureClick: () -> Unit,
+) {
+    var hasCamPermission by remember {
+        mutableStateOf(
+            REQUIRED_PERMISSIONS.all {
+                ContextCompat.checkSelfPermission(context, it) ==
+                        PackageManager.PERMISSION_GRANTED
+            })
+    }
+
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions(),
+        onResult = { granted ->
+            hasCamPermission =granted.size == 2
+        }
+    )
+    LaunchedEffect(key1 = true) {
+        launcher.launch(
+            arrayOf(
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        )
+    }
+    Column(modifier = Modifier.fillMaxSize()) {
+        if (hasCamPermission) {
+            AndroidView(
+                modifier = Modifier.fillMaxSize(),
+                factory = { cameraX.startCameraPreviewView() }
+            )
+        }
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(), Arrangement.Bottom, Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = onCaptureClick
+        ) {
+            Text(text = "Capture")
+        }
     }
 }
